604e28a231fb13b1bb8ea3d6d913dc0a02d51e6e
Mohd Asad Khan <mohdasadkhan666@gmail.com>
2021-08-06 18:49:32 +0530

    Settling Chaneges

---
+++ Learning Web Development/Lecture 7/JS_theory.js
@@ -0,0 +1,14 @@
+/** @format */
+
+console.log("this is line 3", a);
+console.log("this is line 4", b);
+var a, b;
+a = 10;
+b = [1, 2, 3, 4, 5];
+console.log("this is line 8", a);
+console.log("this is line 9", b);
+fn();
+function fn() {
+  console.log("this is function");
+}
+fn();
